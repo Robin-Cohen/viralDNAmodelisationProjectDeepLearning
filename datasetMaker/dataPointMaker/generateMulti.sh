c3c3f274fc84518ffcc6fee6955f8307cb8b6625
Robin <robincohen51@gmail.com>
2025-02-27 16:21:48 +0100

    fix bug

--- datasetMaker/dataPointMaker/generateMulti.sh
+++ datasetMaker/dataPointMaker/generateMulti.sh
@@ -9,17 +9,17 @@ mkdir "$dirRandom/"
 cd "$dirRandom/"
 mkdir -p ../data
 #one call wil generate 10 random spiralDNA
-for i in {10}
+for i in {1..10}
 do
     center="$(generate_random_number -20 20).00 $(generate_random_number -20 20).00 $(generate_random_number -20 20).00"
-    radius=$(generate_random_number 10 50)
+    radius=$(generate_random_number 1 50)
     cp ../workParameter/* .
     sed -i "s/spiralDNA center i/spiralDNA center $center/" "command.generateSpiral.dat"
+    sed -i "s/spiralDNA radius j/spiralDNA radius 0.5/" "command.generateSpiral.dat"
     sed -i "s/spiralDNA radius j/spiralDNA radius $radius\.00/" "command.generateSpiral.dat"
     MMB -c command.generateSpiral.dat 
-    # cat command.generateSpiral.dat
     center=$(echo "$center" | sed 's/ /_/g')
-    mv last.2.cif ../data/"spiralDNAcenter$center-radius$radius.cif"
+    mv last.2.pdb ../data/"spiralDNAcenter$center-radius$radius.pdb"
     
 done
 cd ..
--- datasetMaker/dataPointMaker/multi.sh
+++ datasetMaker/dataPointMaker/multi.sh
@@ -1,13 +1,13 @@
 for ((i=0; i<=$1; i++))
 do
-        bash generateMulti.sh > out_$i.log &
+        bash generateMulti.sh >> out.log &
 done
-wait
-#compile all log file into one
-echo "----------------------------------------------------------------------new generation----------------------------------------------------------------------" >> out.log
-for ((i=0; i<=$1; i++))
-do
-        echo "----------------------------------------------------------------------" >> out.log
-        cat out_$i.log >> out.log
-done
-rm out_*.log
+# wait
+# #compile all log file into one
+# echo "----------------------------------------------------------------------new generation----------------------------------------------------------------------" >> out.log
+# for ((i=0; i<=$1; i++))
+# do
+#         echo "----------------------------------------------------------------------" >> out.log
+#         cat out_$i.log >> out.log
+# done
+# rm out_*.log
