3b5a7ece5b99c4d3beaac438444c6827115df0a4
Robin <robincohen51@gmail.com>
2025-03-17 15:18:13 +0100

    add automatisation of generating data point by modfying parameter

--- datasetMaker/dataPointMaker/generateMultiWithParam.sh
+++ datasetMaker/dataPointMaker/generateMultiWithParam.sh
@@ -1,12 +1,14 @@
 #\bin\bash
 if [ "$1" == "" ]; then
-        source ../parameter.sh
+        source ../parameter
 else
-        fileByProcess=$1
+        fileByProcess=10
         centerMin=20
         centerMax=20
         radiusMin=1
         radiusMax=50
+        pitchMin=1
+        pitchMax=30
 
 fi
 
@@ -15,24 +17,44 @@ generate_random_number() {
     local max=$2
     echo $((RANDOM % (max - min + 1) + min))
 }
-dirRandom="tmp$RANDOM"
-mkdir "$dirRandom/"
-cd "$dirRandom/"
+
+#try to create a directory, if it fail, try again
+generate_a_work_dir() {
+    local dirRandom="tmp$RANDOM"
+    if mkdir "$dirRandom/"; then
+        echo "$dirRandom"
+    else
+        generate_a_work_dir
+    fi
+    
+}
+
+dirRandom=$(generate_a_work_dir)
+cd $dirRandom
+#create a data directory if it doesn't exist
 mkdir -p ../data
 #one call wil generate n(define in $fileByProcess) random spiralDNA
-for i in {1..$fileByProcess}
+for ((i=0; i<=$fileByProcess; i++))
 do
+    echo "-----------------------------------"
+    echo "i: $i"
     #generation of center and radius(only thing that change in this stepp)
     center="$(generate_random_number $centerMin $centerMax).00 $(generate_random_number $centerMin $centerMax).00 $(generate_random_number $centerMin $centerMax).00"
     radius=$(generate_random_number $radiusMin $radiusMax)
+    pitch=$(generate_random_number $pitchMin $pitchMax)
+    pitch=$(echo "$pitch * 0.1" | bc)
+
+    echo "center: $center"
+    echo "radius: $radius"
+    echo "pitch: $pitch"
     cp ../workParameter/* .
-    sed -i "s/spiralDNA center i/spiralDNA center $center/" "command.generateSpiral.dat"
-    sed -i "s/spiralDNA radius j/spiralDNA radius 0.5/" "command.generateSpiral.dat"
-    sed -i "s/spiralDNA radius j/spiralDNA radius $radius\.00/" "command.generateSpiral.dat"
+    sed -i "s/spiralDNA center spiralCenterValues/spiralDNA center $center/" "command.generateSpiral.dat"
+    # sed -i "s/spiralDNA radius spiralRadiusValue/spiralDNA radius 0.5/" "command.generateSpiral.dat"
+    sed -i "s/spiralDNA radius spiralRadiusValue/spiralDNA radius $radius\.00/" "command.generateSpiral.dat"
+    sed -i "s/spiralDNA pitch pitchValue/spiralDNA pitch $pitch/" "command.generateSpiral.dat"
     MMB -c command.generateSpiral.dat 
     center=$(echo "$center" | sed 's/ /_/g')
-    mv last.2.pdb ../data/"spiralDNAcenter$center-radius$radius.pdb"
-    
+    gemmi convert last.2.cif ../data/"spiralDNAcenter$center-radius$radius-pitch$pitch.pdb"
 done
 cd ..
 rm -rf "$dirRandom"
--- datasetMaker/dataPointMaker/multi_param.sh
+++ datasetMaker/dataPointMaker/multi_param.sh
@@ -1,19 +1,19 @@
 if [ "$1" == "" ]; then
-        source ../parameter.sh
+        source ../parameter
 else
         numProcess=$1
 fi
 
+for ((i=0; i<=numProcess; i++))
+do
+        bash generateMultiWithParam.sh >> out.log &
+done
+wait
+# #compile all log file into one
+echo "----------------------------------------------------------------------new generation----------------------------------------------------------------------" >> out.log
 for ((i=0; i<=$1; i++))
 do
-        bash generateMulti.sh >> out.log &
+        echo "----------------------------------------------------------------------" >> out.log
+        cat out_$i.log >> out.log
 done
-# wait
-# #compile all log file into one
-# echo "----------------------------------------------------------------------new generation----------------------------------------------------------------------" >> out.log
-# for ((i=0; i<=$1; i++))
-# do
-#         echo "----------------------------------------------------------------------" >> out.log
-#         cat out_$i.log >> out.log
-# done
-# rm out_*.log
+rm out_*.log
