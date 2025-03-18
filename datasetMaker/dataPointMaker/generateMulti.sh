98505a3379d6a3fb53903ef168856dca31986fe2
Robin <robincohen51@gmail.com>
2025-03-18 14:28:32 +0100

    fix

--- datasetMaker/dataPointMaker/generateMulti.sh
+++ datasetMaker/dataPointMaker/generateMulti.sh
@@ -5,7 +5,7 @@ generate_random_number() {
     echo $((RANDOM % (max - min + 1) + min))
 }
 dirRandom="tmp$RANDOM"
-mkdir "$dirRandom/"
+mkdir -p "$dirRandom/"
 cd "$dirRandom/"
 mkdir -p ../data
 #one call wil generate 10 random spiralDNA
@@ -14,9 +14,10 @@ do
     center="$(generate_random_number -20 20).00 $(generate_random_number -20 20).00 $(generate_random_number -20 20).00"
     radius=$(generate_random_number 1 50)
     cp ../workParameter/* .
-    sed -i "s/spiralDNA center i/spiralDNA center $center/" "command.generateSpiral.dat"
-    sed -i "s/spiralDNA radius j/spiralDNA radius 0.5/" "command.generateSpiral.dat"
-    sed -i "s/spiralDNA radius j/spiralDNA radius $radius\.00/" "command.generateSpiral.dat"
+    sed -i "s/spiralDNA center spiralCenterValues/spiralDNA center $center/" "command.generateSpiral.dat"
+    sed -i "s/spiralDNA radius spiralRadiusValue/spiralDNA radius 0.5/" "command.generateSpiral.dat"
+    sed -i "s/spiralDNA radius spiralRadiusValue/spiralDNA radius $radius\.00/" "command.generateSpiral.dat"
+    
     MMB -c command.generateSpiral.dat 
     center=$(echo "$center" | sed 's/ /_/g')
     mv last.2.pdb ../data/"spiralDNAcenter$center-radius$radius.pdb"
