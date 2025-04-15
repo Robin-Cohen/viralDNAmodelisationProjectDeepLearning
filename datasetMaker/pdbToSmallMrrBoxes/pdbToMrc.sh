3a4a980c39436698fb344ada338f960cb9606bcc
Robin <robincohen51@gmail.com>
2025-04-15 14:26:41 +0200

    add recognition of end/start point

--- datasetMaker/pdbToSmallMrrBoxes/pdbToMrc.sh
+++ datasetMaker/pdbToSmallMrrBoxes/pdbToMrc.sh
@@ -1,34 +1,13 @@
 mkdir -p fullMrcData
+mkdir -p boxData
 cd fullMrcData
-for file in /home/robin/viralDNAmodelisationProjectDeepLearning/datasetMaker/dataPointMaker/data/*
+for file in ../../dataPointMaker/data/*
 do
     base=$(basename "$file" .pdb)
     output_file="${base}.mrc"
     cat ../pdb2volParameter| pdb2vol $file $output_file &
+    python ../downsize.py -output_dir ../boxData/ -input_pdb $file -input_mrc $output_file 
 done
 wait
-python ../downsize.py -output_dir ../boxData *.mrc
-#To do : parallelise the process
-
-# dirRandom="tmp$RANDOM"
-# mkdir "$dirRandom/"
-# cd "$dirRandom/"
-# mkdir -p ../data
-# #one call wil generate 10 random spiralDNA
-# for i in {1..10}
-# do
-#     center="$(generate_random_number -20 20).00 $(generate_random_number -20 20).00 $(generate_random_number -20 20).00"
-#     radius=$(generate_random_number 1 50)
-#     cp ../workParameter/* .
-#     sed -i "s/spiralDNA center i/spiralDNA center $center/" "command.generateSpiral.dat"
-#     sed -i "s/spiralDNA radius j/spiralDNA radius 0.5/" "command.generateSpiral.dat"
-#     sed -i "s/spiralDNA radius j/spiralDNA radius $radius\.00/" "command.generateSpiral.dat"
-#     MMB -c command.generateSpiral.dat 
-#     # cat command.generateSpiral.dat
-#     center=$(echo "$center" | sed 's/ /_/g')
-#     mv last.2.pdb ../data/"spiralDNAcenter$center-radius$radius.pdb"
-#     # mv last.2.pdb ../data/"spiralDNAcenter$center-radius0.5.pdb"
-    
-# done
-# cd ..
-# rm -rf "$dirRandom"
+# python ../downsize.py -output_dir ../boxData *.mrc
+rm -rf fullMrcData
