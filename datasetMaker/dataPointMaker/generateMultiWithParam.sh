bb074a62e58cb2be791e8a97a9cf8849b8ae6962
Robin <robincohen51@gmail.com>
2025-05-07 11:16:28 +0200

    fix small file path def

--- datasetMaker/dataPointMaker/generateMultiWithParam.sh
+++ datasetMaker/dataPointMaker/generateMultiWithParam.sh
@@ -1,4 +1,9 @@
 #\bin\bash
+# This script generates multiple spiral DNA structures with random parameters and saves them in a specified directory.
+# It also creates a CSV file containing the parameters used for each generated structure.
+# Check if the script is being run with a parameter
+# If no parameter is provided, source the default parameter file
+# If a parameter is provided, use the provided values
 if [ "$1" == "" ]; then
         source ../parameter
 else
@@ -9,13 +14,15 @@ else
         radiusMax=50
         pitchMin=1
         pitchMax=30
-
+        # rightHanded=1
 fi
 
 generate_random_number() {
+    #redo after discover of shuff function--> will be replace
     local min=$1
     local max=$2
-    echo $((RANDOM % (max - min + 1) + min))
+    # echo $((RANDOM % (max - min + 1) + min))
+    shuf -i $min-$max -n 1
 }
 
 #try to create a directory, if it fail, try again
@@ -31,6 +38,8 @@ generate_a_work_dir() {
 
 dirRandom=$(generate_a_work_dir)
 cd $dirRandom
+# Write labels to a CSV file
+    csv_file="../labels.csv"
 #create a data directory if it doesn't exist
 mkdir -p ../data
 #one call wil generate n(define in $fileByProcess) random spiralDNA
@@ -39,24 +48,49 @@ do
     echo "-----------------------------------"
     echo "i: $i"
     #generation of center and radius(only thing that change in this stepp)
-    center="$(generate_random_number $centerMin $centerMax).00 $(generate_random_number $centerMin $centerMax).00 $(generate_random_number $centerMin $centerMax).00"
-    radius=$(generate_random_number $radiusMin $radiusMax)
-    pitch=$(generate_random_number $pitchMin $pitchMax)
-    pitch=$(echo "$pitch * 0.1" | bc)
-    phi=$(generate_random_number 0 360)
-
-    echo "center: $center"
+    centerx="$(seq -f'%.2f' -50 0.1 50 | shuf -n1)"
+    centery="$(seq -f'%.2f' -50 0.1 50 | shuf -n1)"
+    centerz="$(seq -f'%.2f' -50 0.1 50 | shuf -n1)"
+    radius="$(seq -f'%.2f' $radiusMin 0.1 $radiusMax | shuf -n1)"
+    # radius=$(echo "$radius * 0.1" | bc)
+    # radius=10
+    pitch="$(seq -f'%.2f' $pitchMin 0.1 $pitchMax | shuf -n1)"
+    # pitch=$(echo "$pitch * 0.1" | bc)
+    # pitch=3
+    phi="$(seq -f'%.2f' 0 1 360 | shuf -n1)"
+    # phi=319
+    rightHanded="$(seq -f '%.0f' 0 1|shuf -n1)"
+    # theta1or=31
+    #teta0 and teta1 are between 0 and 25
+    # theta0="$(seq -f'%.3f' 0 0.01 3.14 | shuf -n1)"
+    # theta1="$(seq -f'%.3f' 0 0.01 3.14 | shuf -n1)"
+    theta0=1.0
+    theta1=2.2
+    # echo "center: $center"
     echo "radius: $radius"
     echo "pitch: $pitch"
+    echo "phi: $phi"
+    echo "teta0: $theta0"
+    echo "teta1: $theta1"
+    echo "rightHanded: $rightHanded"
     cp ../workParameter/* .
-    sed -i "s/spiralDNA center spiralCenterValues/spiralDNA center $center/" "command.generateSpiral.dat"
-    # sed -i "s/spiralDNA radius spiralRadiusValue/spiralDNA radius 0.5/" "command.generateSpiral.dat"
-    sed -i "s/spiralDNA radius spiralRadiusValue/spiralDNA radius $radius\.00/" "command.generateSpiral.dat"
-    sed -i "s/spiralDNA pitch pitchValue\/spiralDNA pitch $pitch/" "command.generateSpiral.dat"
-    # sed -i "s/spiralDNA phiOffset  phivar*@pi/180/spiralDNA phiOffset  $phi*@pi/180/" "command.generateSpiral.dat"
-    MMB -c command.generateSpiral.dat 
-    center=$(echo "$center" | sed 's/ /_/g')
-    gemmi convert last.2.cif ../data/"spiralDNAcenter$center-radius$radius-pitch$pitch-phi$phi.pdb"
+    sed -i "s/spiralDNA center spiralCenterValues/spiralDNA center $centerx $centery $centerz/" "command.generateSpiral.dat"
+    sed -i "s/spiralDNA radius spiralRadiusValue/spiralDNA radius $radius/" "command.generateSpiral.dat"
+    sed -i "s/spiralDNA pitch pitchValue/spiralDNA pitch $pitch/" "command.generateSpiral.dat"
+    sed -i "s/spiralDNA phivar/spiralDNA phiOffset  $phi*@pi\/180/" "command.generateSpiral.dat"
+    sed -i "s/spiralDNA startTheta/spiralDNA startTheta $theta0/" "command.generateSpiral.dat"
+    sed -i "s/spiralDNA endTheta/spiralDNA endTheta $theta1/" "command.generateSpiral.dat"
+    sed -i "s/spiralDNA spiralIsRightHanded/spiralDNA spiralIsRightHanded $rightHanded/" "command.generateSpiral.dat"
+    cat command.generateSpiral.dat
+
+ 
+    MMB -c command.generateSpiral.dat > tmp.log
+    # center=$(echo "$center" | sed 's/ /_/g')
+    fileName="spiralDNAcenter$centerx-$centery-$centerz-rightHanded$rightHanded-radius$radius-pitch$pitch-phi$phi-teta0-$theta0-teta1-$theta1.pdb"
+    gemmi convert last.2.cif ../data/$fileName
+    
+
+    echo "$fileName,$centerx,$centery,$centerz,$rightHanded,$radius,$pitch,$phi,$theta0,$theta1" >> "$csv_file"
 done
 cd ..
 rm -rf "$dirRandom"
--- datasetMaker/dataPointMaker/multi_param.sh
+++ datasetMaker/dataPointMaker/multi_param.sh
@@ -1,3 +1,7 @@
+# Write labels to a CSV file
+csv_file="labels.csv"
+echo "fileName,centerx,centery,centerz,rightHanded,radius,pitch,phi,theta0,theta1" > "$csv_file"
+    
 if [ "$1" == "" ]; then
         source ../parameter
 else
@@ -9,11 +13,11 @@ do
         bash generateMultiWithParam.sh >> out.log &
 done
 wait
-# #compile all log file into one
-echo "----------------------------------------------------------------------new generation----------------------------------------------------------------------" >> out.log
-for ((i=0; i<=$1; i++))
-do
-        echo "----------------------------------------------------------------------" >> out.log
-        cat out_$i.log >> out.log
-done
-rm out_*.log
+# # #compile all log file into one
+# echo "----------------------------------------------------------------------new generation----------------------------------------------------------------------" >> out.log
+# for ((i=0; i<=$1; i++))
+# do
+#         echo "----------------------------------------------------------------------" >> out.log
+#         cat out_$i.log >> out.log
+# done
+# rm out_*.b
