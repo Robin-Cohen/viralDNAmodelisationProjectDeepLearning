81b5fe3ab9cced575fa1e26e7c395bbfa4b7c106
Robin-Cohen <94676585+Robin-Cohen@users.noreply.github.com>
2025-06-10 09:44:03 +0200

    Delete RecurrentModel directory

--- RecurrentModel/devTest.py
+++
@@ -1,8 +0,0 @@
-from devModel import *
-
-pthName= "/home/robin/viralDNAmodelisationProjectDeepLearning/model.pth"
-mrcRep="/home/robin/viralDNAmodelisationProjectDeepLearning/RecurrentModel/testmrc"
-
-model=Model()
-model.load_state_dict(torch.load(pthName))
-model.eval()
